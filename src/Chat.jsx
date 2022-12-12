0b496d6d4c0222cc455cc2050985d32b00f588c2
neexal <nishanghimire111@gmail.com>
2022-12-12 16:40:43 +0545

    commit2

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -2,45 +2,51 @@ import React from 'react'
 // import { AiOutlineMenu } from 'react-icons'
 
 const Chat = () => {
-    // const [msg, setMsg] = React.useState('');
-    const [message, setMessage] = React.useState([]);
-    const addMessages=(msg)=>{
-        setMessage([...message, msg])
-        console.log(message)
+  // const [msg, setMsg] = React.useState('');
+  const [message, setMessage] = React.useState([]);
+  const addMessages = (msg) => {
+    setMessage([...message, msg])
+    console.log(message)
 
-    }
+  }
 
-    const handleSubmit=(e)=>{
-        e.preventDefault()
-        const msgInput = e.target.elements.message;
-        const msg = msgInput.value;
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const msgInput = e.target.elements.message;
+    const msg = msgInput.value;
 
-        addMessages(msg);
-        msgInput.value=''
-    }
+    addMessages(msg);
+    msgInput.value = ''
+  }
 
-    return (
-        <form onSubmit={handleSubmit}>
-        <div className="chat-container">
-          <div className="chat-header">
-            <h3>Chat</h3>
-          </div>
-          <div className="chat-messages">
-            <div className="message-container sent">
-              <p>Hello!</p>
-            </div>
-            <div className="message-container received">
-              <p>Hi there!</p>
-            </div>
-          </div>
-          <div className="chat-input">
-            <input type="text" name="message" placeholder="Enter your message here" />
-            <button>Send</button>
-          </div>
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="chat-container">
+        <div className="chat-header">
+          <h3>Chat</h3>
         </div>
-        </form>
-      );
-    }
+        <div className="chat-messages">
+          {
+            message.map((res) => {
+              return (
+                <div className="message-container sent">
+                  <p>Hello!</p>
+                </div>
+              )
+            })
+          }
+          {/* <div className="message-container received">
+              <p>Hi there!</p>
+            </div> */}
+        </div>
+        <div className="chat-input">
+          <input type="text" name="message" placeholder="Enter your message here" />
+          <button>Send</button>
+        </div>
+      </div>
+    </form>
+  );
+}
 
 
 export default Chat
